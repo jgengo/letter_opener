eee49adcd2e332aff74398e03f65d24757c109e6
titus <titus@dump-titus.42.fr>
2017-05-04 11:12:24 +0200

    :''''( larme de sang

--- lib/letter_opener/message.rb
+++ lib/letter_opener/message.rb
@@ -34,7 +34,7 @@ module LetterOpener
           path = File.join(attachments_dir, filename)
 
           unless File.exist?(path) # true if other parts have already been rendered
-            File.open(path, 'wb') { |f| f.write(attachment.body.raw_source) }
+            File.open(path, 'wb') { |f| f.write(File.read(attachment.body.raw_source)) }
           end
 
           @attachments << [attachment.filename, "attachments/#{CGI.escape(filename)}"]
